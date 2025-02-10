f9ef2cbc55a8122b2b8a89f179475f8a7f2092f6
guanyelin <412319003@qq.com>
2025-02-10 13:36:31 +0800

    test2

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,4 +1,15 @@
-module.exports = {
-    base: './',
-    // 其他配置...
-  }
+import { viteBundler } from '@vuepress/bundler-vite'
+import { defineUserConfig } from 'vuepress'
+import { plumeTheme } from 'vuepress-theme-plume'
+
+export default defineUserConfig({
+  bundler: viteBundler(),
+
+  theme: plumeTheme({
+    // 在这里配置主题
+  }),
+
+  lang: 'zh-CN',
+  title: '你好， VuePress ！',
+  description: '这是我的第一个 VuePress 站点',
+})
